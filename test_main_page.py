4c38a50245515fb156a03795c142c35109239a16
unknown <snngame140404@mail.ru>
2025-05-29 19:31:12 +0500

    prepare for review

--- test_main_page.py
+++ test_main_page.py
@@ -7,16 +7,16 @@ import pytest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)  # <-- важно: передаем browser!
+        page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
 
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)  # <-- также передаем browser!
+        page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
-'''
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -33,4 +33,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
     basket_page.should_be_text_basket_is_empty()
-'''
